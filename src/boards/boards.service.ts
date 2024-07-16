8dedf968fa5331f51074e1eea095c693485a0443
sim <simyechan87@gmail.com>
2024-07-16 16:55:10 +0900

    예외처리 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -18,19 +18,26 @@ export class BoardsService {
       id: uuid(),
       title,
       description,
-      status: BoardStatus.PUBLIC
-    }
+      status: BoardStatus.PUBLIC,
+    };
 
-    this.boards.push(board)
+    this.boards.push(board);
     return board;
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return found;
   }
 
   deletBoard(id: string): void {
-    this.boards = this.boards.filter((board) => board.id !== id);
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
   }
 
   updateBoardStatus(id: string, status: BoardStatus): Board {
