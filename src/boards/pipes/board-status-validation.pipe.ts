45c168a47b4d5ea3d955bfa9eefdf7c0e9b54eb8
sim <simyechan87@gmail.com>
2024-07-18 17:06:31 +0900

    커스텀 파이프 유효성 체크

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOption = [
+    BoardStatus.PRIVATE,
+    BoardStatus.PUBLIC
+  ]
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if(!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+    
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOption.indexOf(status);
+    return index !== -1
+  }
+}
