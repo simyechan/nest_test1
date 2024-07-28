e34e336fd54df71032611e4e4b2d84ad8783f6b6
sim <simyechan87@gmail.com>
2024-07-28 15:39:33 +0900

    TypeORM 게시물 추가

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,23 +1,20 @@
-import { BadRequestException, PipeTransform } from "@nestjs/common";
-import { BoardStatus } from "../board.model";
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board-status.enum';
 
 export class BoardStatusValidationPipe implements PipeTransform {
-  readonly StatusOption = [
-    BoardStatus.PRIVATE,
-    BoardStatus.PUBLIC
-  ]
+  readonly StatusOption = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
   transform(value: any) {
     value = value.toUpperCase();
 
-    if(!this.isStatusValid(value)) {
+    if (!this.isStatusValid(value)) {
       throw new BadRequestException(`${value} isn't in the status options`);
     }
-    
+
     return value;
   }
 
   private isStatusValid(status: any) {
     const index = this.StatusOption.indexOf(status);
-    return index !== -1
+    return index !== -1;
   }
 }
