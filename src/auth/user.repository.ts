7d300639cbbeda1994b3ac8fe136246e1742e84c
sim <simyechan87@gmail.com>
2024-07-28 17:25:54 +0900

    회원가입 수정

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,8 @@
 import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -11,8 +12,20 @@ export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
 
-    await this.save(user);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if(error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
